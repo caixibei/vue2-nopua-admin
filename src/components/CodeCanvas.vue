8b82d137ecf378527e73a559cad95756de45ead4
caixibei@139.com <caixibei@139.com>
2023-06-16 16:17:53 +0800

    验证码节流

--- src/components/CodeCanvas.vue
+++ src/components/CodeCanvas.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="el-canvas" @click="draw([])">
+	<div class="el-canvas" @click="throttleDraw">
 		<canvas
 			:ref="canvasId"
 			:width="canvasWidth"
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import _ from "loadsh";
 export default {
 	name: "code-canvas",
 	props: {
@@ -27,43 +28,48 @@ export default {
 			default: "50",
 		},
 	},
-    data(){
-        return {
-            codeText:'',
-            maskStyle:"--content:0",
-            maskTime:60
-        }
-    },
-    mounted(){
-        let _this = this
-        let _ = require('loadsh')
-        _this.$nextTick(function(){
-            _this.draw([]);
-            _.debounce(_this.draw,1000)
-        });
-    },
+	data() {
+		return {
+			codeText: "",
+		};
+	},
+	mounted() {
+		let _this = this;
+		_this.$nextTick(function () {
+			_this.throttleDraw();
+		});
+	},
 	methods: {
-		randomColor:function() {
+		randomColor: function () {
 			var r = Math.floor(Math.random() * 256);
 			var g = Math.floor(Math.random() * 256);
 			var b = Math.floor(Math.random() * 256);
 			return "rgb(" + r + "," + g + "," + b + ")";
 		},
-		draw:function(show_num) {
-            let _this = this
-            _this.codeText = '';
+		throttleDraw: _.throttle(
+			function () {
+				let _this = this;
+				_this.draw([]);
+			},
+			3000,
+			{ leading: false }
+		),
+		draw: function (show_num) {
+			let _this = this;
+			_this.codeText = "";
 			var canvas_width = _this.canvasWidth;
 			var canvas_height = _this.canvasHeight;
 			var canvas = _this.$refs[_this.canvasId];
 			var context = canvas.getContext("2d");
 			canvas.width = canvas_width;
 			canvas.height = canvas_height;
-			var sCode = "a,b,c,d,e,f,g,h,i,j,k,m,n,p,q,r,s,t,u,v,w,x,y,z,A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
+			var sCode =
+				"a,b,c,d,e,f,g,h,i,j,k,m,n,p,q,r,s,t,u,v,w,x,y,z,A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
 			var aCode = sCode.split(",");
 			var aLength = aCode.length;
-            // 验证码文本内容
+			// 验证码文本内容
 			for (var i = 0; i < 4; i++) {
-				var j = Math.floor(Math.random() * aLength); 
+				var j = Math.floor(Math.random() * aLength);
 				var deg = Math.random() - 0.5; //产生一个随机弧度
 				var txt = aCode[j]; //得到随机的一个内容
 				show_num[i] = txt.toLowerCase();
@@ -76,9 +82,9 @@ export default {
 				context.fillText(txt, 0, 0);
 				context.rotate(-deg);
 				context.translate(-x, -y);
-                _this.codeText += ''+txt;
+				_this.codeText += "" + txt;
 			}
-            //验证码上显示线条
+			//验证码上显示线条
 			for (let i = 0; i <= 5; i++) {
 				context.strokeStyle = this.randomColor();
 				context.beginPath();
@@ -92,7 +98,7 @@ export default {
 				);
 				context.stroke();
 			}
-            //验证码上显示小点
+			//验证码上显示小点
 			for (let i = 0; i <= 30; i++) {
 				context.strokeStyle = this.randomColor();
 				context.beginPath();
@@ -102,7 +108,7 @@ export default {
 				context.lineTo(x + 1, y + 1);
 				context.stroke();
 			}
-            _this.$emit('draw-code', _this.codeText);
+			_this.$emit("draw-code", _this.codeText);
 		},
 	},
 };
